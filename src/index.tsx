aeaee802ed6b77d034fd0ffefbefe224489816fd
dellman000 <kenren@udel.edu>
2021-10-21 11:17:36 -0400

    add Headera

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import Tab from "./Tab";
+import Header from "./Header";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <Header/>
+        
+        {/*<App /> */}
     </React.StrictMode>,
     document.getElementById("root")
 );
