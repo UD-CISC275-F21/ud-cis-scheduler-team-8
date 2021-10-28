c1aeec5ae0c561459ea21c001403f8601b73ce4e
Feitong Liu <feitong@udel.edu>
2021-10-28 15:12:53 -0400

    import courses

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import "./App.css";
 import Tab from "./Component/Table";
 import Course_pull from "./Component/Course_pool";
+import Courses from "./assets/coursedata.json";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+
+
 function App(): JSX.Element {
+
     return (
         <div> 
             <Course_pull/>
