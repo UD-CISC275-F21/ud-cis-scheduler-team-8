c245334ae22cd955c3f3da0060f92a011ff47259
Feitong Liu <feitong@udel.edu>
2021-10-28 15:18:05 -0400

    delete import course

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./App.css";
 import Tab from "./Component/Table";
 import Course_pull from "./Component/Course_pool";
-import Courses from "./assets/coursedata.json";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 
