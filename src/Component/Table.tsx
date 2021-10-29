7ed1a6149292f0e82eb01e92d5ae90e7f77e7beb
Shaozhe Li <lsz@udel.edu>
2021-10-29 11:18:10 -0400

    solve the conflict

--- src/Component/Table.tsx
+++ src/Component/Table.tsx
@@ -173,7 +173,7 @@ function Tab({currentCourse, setCurrentCourse, deck}:
                     <td>ENGL110 <button><IoRemoveCircleOutline /></button></td>
                     <td className="click" onClick={()=> {
                         setCurrentCourse(COURSES[2]); setShow(true);
-                    }}>CISC210 <button><IoRemoveCircleOutline /></button></td>
+                    }}>CISC210 </td>
                 </tr>
             </tbody>
         </Table>
