60167088a141b9a68a4b336921905dfa8f888881
Shaozhe Li <lsz@udel.edu>
2021-11-12 15:36:24 -0500

    make a part of the semester

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,15 @@ import CoursePool from "./Component/CoursePool";
 import Header from "./Header";
 import COURSES from "./assets/courses.json";
 import "bootstrap/dist/css/bootstrap.min.css";
-
 import { Container, Row } from "react-bootstrap";
-import { Course } from "./interfaces/courses";
+import { Course, Semester } from "./interfaces/courses";
 
 function App(): JSX.Element {
 
     const [pool, setPool]=useState <Course[]>(COURSES); 
     const [schedule,setSchedule] = useState <Course[]>(COURSES);
     const [ActiveCourse, SetActiveCourse] = useState<Course>(COURSES[0]);
+    const [semesters, setSemesters] = useState<Semester[]>([]);
 
     return (
         <Container className="App">
@@ -25,7 +25,9 @@ function App(): JSX.Element {
                     schedule={schedule}
                     setSchedule={setSchedule}
                     setCourse={SetActiveCourse}
-                    course={ActiveCourse}></Tab>
+                    course={ActiveCourse}
+                    semesters={semesters}
+                    setSemesters={setSemesters}></Tab>
                 <CoursePool pool={pool} setPool={setPool}></CoursePool>
             </Row>
 
--- src/Component/Table.tsx
+++ src/Component/Table.tsx
@@ -3,12 +3,67 @@ import React from "react";
 import { IoRemoveCircleOutline } from "react-icons/io5";
 import "bootstrap/dist/css/bootstrap.min.css";
 import {Button, Col} from "react-bootstrap";
-import {Course} from "../interfaces/courses";
+import {Course, Semester} from "../interfaces/courses";
 import COURSES from "../assets/courses.json";
 
-export function Tab({setCourse,schedule,setSchedule}:
-    {setCourse:(c:Course)=>void, schedule:Course[], setSchedule:(s:Course[])=>void, course:Course}): JSX.Element {
-    return <Col className="planner">
+export function Tab({setCourse,course,schedule,setSchedule,semesters,setSemesters}:
+    {setCourse:(c:Course)=>void, schedule:Course[], setSchedule:(s:Course[])=>void, course:Course
+    semesters:Semester[],setSemesters:(se:Semester[])=>void }): JSX.Element {
+
+
+
+    const semesterItems = semesters.map(function (semester: Semester): JSX.Element {
+        let courseid= semester.courses[0].ID;
+        let coursename= semester.courses[0].Name;
+        for (let i = 0; i < 14; i++) {
+            courseid = semester.courses[i].ID;
+            console.log(courseid);
+            coursename= semester.courses[i].Name;
+            console.log(coursename);
+        }
+        return <div key={courseid}>
+            <div>
+                <Table className="freshman">
+                    <tr>
+                        <td>{coursename.toUpperCase()}
+                            <IoRemoveCircleOutline onClick = {()=>{
+                                //deleteButton();
+                            }}/></td>
+                        <td>{coursename.toUpperCase()}
+                            <IoRemoveCircleOutline onClick = {()=>{
+                                //deleteButton();
+                            }}/></td>
+                    </tr>
+                </Table>
+            </div>
+        </div>;
+    });
+
+    /**function deleteButton(): void{
+        setSemesters(semesters.filter((oldSemester: Semester): boolean => {
+            return semesters.courses !== oldSemester.courses;
+        }));
+    }*/
+
+    return <Col className="pool">
+        <h2>Schedule</h2>
+        <thead>
+            <tr>
+                <th>FRESHMAN</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Fall<Button>Clear</Button></td>
+                <td>Spring <Button>Clear</Button></td>
+            </tr>
+            {semesterItems}
+        </tbody>
+    </Col>;
+
+
+    
+    /**return <Col className="planner">
         <h2>CISC PLANNER</h2>
         <Table className="freshman">
             <thead>
@@ -107,10 +162,10 @@ export function Tab({setCourse,schedule,setSchedule}:
             </tbody>
         </Table>
     </Col>;
+    */
 
 
 } 
 
 
 export default Tab;
-
