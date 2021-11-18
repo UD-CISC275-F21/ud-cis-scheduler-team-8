0a45c6fce5ac92a012ed031bf27f507696faef41
Feitong Liu <feitong@udel.edu>
2021-11-18 18:25:51 -0500

    fix error

--- src/Component/Usermessage.tsx
+++ src/Component/Usermessage.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Button, Modal } from "react-bootstrap";
-import { JsxEmit } from "typescript";
 
 export function Popup() :JSX.Element{
     const [show, setShow] = useState(false);
